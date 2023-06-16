a456d48d9fe83d7cd1f7856bb43e051909e2401a
Fabio Gomez <fabiogomez11c@gmail.com>
2023-06-15 19:29:04 -0500

    progres with single head attention

--- backend/src/model.py
+++ backend/src/model.py
@@ -5,6 +5,33 @@ from torch.utils.data import DataLoader
 from constant import VOCAB
 
 
+class Head(nn.Module):
+    """one head attention"""
+
+    def __init__(self, head_size, n_embd, block_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.block_size = block_size
+
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)  # (B, T, C)
+        q = self.query(x)  # (B, T, C)
+        # compute attention scores ("affinities")
+        assert self.block_size == T
+        # remember the scaling by dimension to avoid bias in softmax - scale attention
+        wei = q @ k.transpose(-1, -2) * C**-0.5  # (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)  # (B, T, T)
+        # perform the weighted aggregation of the values
+        v = self.value(x)  # (B, T, C)
+        out = wei @ v  # (B, T, C)
+        return out
+
+
 class BigramLanguageModel(nn.Module):
     def __init__(self, n_embd=32, block_size=8, device="cpu"):
         super().__init__()
@@ -12,7 +39,9 @@ class BigramLanguageModel(nn.Module):
         self.itos = {i: c for i, c in enumerate(VOCAB)}
         self.token_embedding_table = nn.Embedding(len(VOCAB), n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd, n_embd, block_size)
         self.lm_head = nn.Linear(n_embd, len(VOCAB))
+        self.block_size = block_size
 
         self.device = device
 
@@ -24,6 +53,7 @@ class BigramLanguageModel(nn.Module):
             torch.arange(T, device=self.device)
         )  # (T, C)
         x = token_emb + pos_emb  # (B, T, C)
+        x = self.sa_head(x)  # (B, T, C)
         logits = self.lm_head(x)  # (B, T, vocab_size)
 
         if targets is None:
@@ -46,6 +76,8 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx = idx[:, -self.block_size :]  # (B, T)
             # get the predictions
             model_result = self(idx)
             if type(model_result) == tuple:
@@ -61,16 +93,3 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
         return idx
-
-
-class BigramLanguageModelV2(BigramLanguageModel):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, idx):
-        logits = self.token_embedding_table(idx)  # (B, T, C)
-
-        if len(logits.shape) == 3:
-            B, T, C = logits.shape
-            logits = logits.view(B * T, C)
-        return logits
