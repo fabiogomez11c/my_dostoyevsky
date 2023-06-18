2bd599314e18e925ccebf56f4b3c4587fa4e6b0b
Fabio Gomez <fabiogomez11c@gmail.com>
2023-06-17 23:32:23 -0500

    changed data to return random chunks

--- backend/src/data.py
+++ backend/src/data.py
@@ -1,21 +1,30 @@
 import torch
-from model import VOCAB
+from src.model import VOCAB
 
 
 class FyodorDataset(torch.utils.data.Dataset):
-    def __init__(self, all_books: str):
+    def __init__(
+        self,
+        all_books: str,
+        block_size: int = 8,
+        batch_size: int = 32,
+        length: int = 1000,
+    ):
         self.stoi = {c: i for i, c in enumerate(VOCAB)}
         self.itos = {i: c for i, c in enumerate(VOCAB)}
         self.data = torch.tensor(self.encode(all_books), dtype=torch.long)
+        self.block_size = block_size
+        self.batch_size = batch_size
+        self.length = length
 
     def __len__(self):
-        return len(self.data)
+        return self.length
 
     def __getitem__(self, idx):
-        if idx >= len(self.data) - 1:
-            return self.data[idx - 2], self.data[idx - 1]
-        else:
-            return self.data[idx], self.data[idx + 1]
+        ix = torch.randint(len(self.data) - self.block_size, (1,))
+        x = torch.stack([self.data[i : i + self.block_size] for i in ix])
+        y = torch.stack([self.data[i + 1 : i + self.block_size + 1] for i in ix])
+        return x[0], y[0]
 
     def encode(self, x):
         return [self.stoi[c] for c in x]
