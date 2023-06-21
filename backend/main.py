d4b0f93cba46b91b45054f9142baf463f3201c3c
Fabio Gomez <fabiogomez11c@gmail.com>
2023-06-20 22:21:44 -0500

    preparing for deployment

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = BigramLanguageModel()
-fyodor = torch.load("model_store/fyodor.pt")
-model.load_state_dict(fyodor["state_dict"])
+model_dir = "model_store/attention_statedict.pth"
+model = BigramLanguageModel(
+    n_embd=384,
+    block_size=256,
+    n_head=4,
+    n_layer=12,
+    dropout=0.3,
+    to_device="cpu",
+)
+fyodor = torch.load(model_dir)
+model.load_state_dict(fyodor)
+model.eval()
 
 
 class Input(BaseModel):
@@ -25,8 +34,8 @@ class Input(BaseModel):
 
 @app.post("/predict")
 def predict(input: Input):
-    encoded = torch.tensor([[model.encode(input.text)[-1]]], dtype=torch.long)
-    encoded_generated = model.generate(encoded, 100)
+    encoded = torch.tensor([model.encode(input.text)], dtype=torch.long)
+    encoded_generated = model.generate(encoded, 500)
     decode = model.decode(encoded_generated[0].tolist())
     return {"text": decode}
 
--- backend/predictor.py
+++ backend/predictor.py
@@ -1,13 +1,12 @@
 import torch
-import mlflow.pytorch
 from src.model import BigramLanguageModel
-from trainer import BigramLightning
 
 
 def predictor(text) -> str:
     # load the model
-    uri = "mlartifacts/927816015437864496/eea158f9f96c4164b0494e3c6ede2d80/artifacts/model"
-    state_dict = mlflow.pytorch.load_state_dict(uri)
+    uri = "mlartifacts/927816015437864496/eea158f9f96c4164b0494e3c6ede2d80/artifacts/model/state_dict.pth"
+    # load state dict
+    state_dict = torch.load(uri)
     model = BigramLanguageModel(
         n_embd=384,
         block_size=256,
@@ -17,9 +16,9 @@ def predictor(text) -> str:
         to_device="cpu",
     )
     model.load_state_dict(state_dict)
-    decoded = torch.tensor([model.encode(text)], dtype=torch.long)
+    encode = torch.tensor([model.encode(text)], dtype=torch.long)
     model.eval()
-    return model.decode(model.generate(idx=decoded, max_new_tokens=500)[0].tolist())
+    return model.decode(model.generate(idx=encode, max_new_tokens=500)[0].tolist())
 
 
 if __name__ == "__main__":
