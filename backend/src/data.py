9926b49fbab67a4d8a424597cb41be933737a512
Fabio Gomez <fabiogomez11c@gmail.com>
2023-07-31 08:45:54 -0500

    fixed issue with data

--- backend/src/data.py
+++ backend/src/data.py
@@ -3,35 +3,22 @@ from src.model import VOCAB
 
 
 class FyodorDataset(torch.utils.data.Dataset):
-    class FyodorDataset(torch.utils.data.Dataset):
-        def __init__(
-            self,
-            all_books: str,
-            block_size: int = 8,
-            batch_size: int = 32,
-            length: int = 1000,
-        ):
-            self.stoi = {c: i for i, c in enumerate(VOCAB)}
-            self.itos = {i: c for i, c in enumerate(VOCAB)}
-            self.data = torch.tensor(self.encode(all_books), dtype=torch.long)
-            self.block_size = block_size
-            self.batch_size = batch_size
-            self.length = length
+    def __init__(
+        self,
+        all_books: str,
+        block_size: int = 8,
+        batch_size: int = 32,
+        length: int = 1000,
+    ):
+        self.stoi = {c: i for i, c in enumerate(VOCAB)}
+        self.itos = {i: c for i, c in enumerate(VOCAB)}
+        self.data = torch.tensor(self.encode(all_books), dtype=torch.long)
+        self.block_size = block_size
+        self.batch_size = batch_size
+        self.length = length
 
-        def __len__(self):
-            return self.length
-
-        def __getitem__(self, idx):
-            ix = torch.randint(len(self.data) - self.block_size, (1,))
-            x = torch.stack([self.data[i : i + self.block_size] for i in ix])
-            y = torch.stack([self.data[i + 1 : i + self.block_size + 1] for i in ix])
-            return x[0], y[0]
-
-        def encode(self, x):
-            return [self.stoi[c] for c in x]
-
-        def decode(self, x):
-            return "".join([self.itos[c] for c in x])
+    def __len__(self):
+        return self.length
 
     def __getitem__(self, idx):
         ix = torch.randint(len(self.data) - self.block_size, (1,))
@@ -44,3 +31,18 @@ class FyodorDataset(torch.utils.data.Dataset):
 
     def decode(self, x):
         return "".join([self.itos[c] for c in x])
+
+
+def __getitem__(self, idx):
+    ix = torch.randint(len(self.data) - self.block_size, (1,))
+    x = torch.stack([self.data[i : i + self.block_size] for i in ix])
+    y = torch.stack([self.data[i + 1 : i + self.block_size + 1] for i in ix])
+    return x[0], y[0]
+
+
+def encode(self, x):
+    return [self.stoi[c] for c in x]
+
+
+def decode(self, x):
+    return "".join([self.itos[c] for c in x])
